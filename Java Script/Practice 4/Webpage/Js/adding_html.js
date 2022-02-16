407e80c9e7cb3cb50676bf2c4e9dfa9d131728ef
Vicky Gupta <63355200+Vicky-Guptaa@users.noreply.github.com>
2022-02-16 21:21:19 +0530

    Delete Java Script directory

--- Java Script/Practice 4/Webpage/Js/adding_html.js
+++
@@ -1,6 +0,0 @@
-const todoList = document.querySelector(".todo-list")
-console.log(todoList.innerHTML);
-
-// todoList.innerHTML = "<li>New Todo</li>";
-todoList.innerHTML += "<li>New Todo</li>";
-todoList.innerHTML += "<li>lorem 2</li>";
--- Java Script/Practice 4/Webpage/Js/classList.js
+++
@@ -1,6 +0,0 @@
-const plan = document.querySelector("section");
-console.log(plan.classList);
-// plan.classList.add("bg-dark");
-// plan.classList.remove("container");
-console.log(plan.classList.contains("container"));
-plan.classList.toggle("bg-dark");
